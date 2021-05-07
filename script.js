e73e16344a725e262bce6511c192075bd5118db3
josephe44 <josephe442@gmail.com>
2021-05-07 10:48:13 +0100

    updating js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // javascript
 
 const menuBtn = document.querySelector(".menu");
-panel = document.querySelector(".filter");
-panelStyles = window.getComputedStyle(panel);
-hearts = document.querySelectorAll(".far");
+const panel = document.querySelector(".filter");
+const panelStyles = window.getComputedStyle(panel);
+const hearts = document.querySelectorAll(".far");
 
 menuBtn.addEventListener("click", () => {
   if (panelStyles.getPropertyValue("clip-path") === "circle(0% at 100% 0%)") {
